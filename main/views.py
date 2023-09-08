a0ff06e411af570505abf36d60565e1b157821e6
shahbakhat <shahbakhat008@gmail.com>
2023-09-09 01:06:24 +0500

    Design Products detail page

--- main/views.py
+++ main/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 def index(request):
   template_name = 'main/index.html'
   return render(request, template_name)
+
+def product_detail(request, id):
+  template_name = 'main/product_detail.html'
+  image_url = f"main/images/product/{id}.jpg"
+  content = {'id': id, 'image_url': image_url}
+  return render(request, template_name, content)
