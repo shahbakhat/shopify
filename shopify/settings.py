b08893eb13726639cf553d2e61506784e45d0555
Shah Bakhat <shahbakhat008@gmail.com>
2023-10-03 20:14:18 +0100

    chnaged the secret key

--- shopify/settings.py
+++ shopify/settings.py
@@ -23,7 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
+from django.core.management.utils import get_random_secret_key
+
+SECRET_KEY = get_random_secret_key()
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
